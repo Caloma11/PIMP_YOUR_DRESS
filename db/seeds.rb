cb729544d882c2a919746d95fb81302c681a70fe
andre rache <andrezra@gmail.com>
2018-04-03 12:36:34 -0300

    added cloudinary and started work on the advisor page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+5.times do
+ User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, password: Faker::Code.asin)
+ Advisor.create(user_id: User.last.id, city:Faker::Address.city, rate: rand(100..200), title: Faker::Lorem.sentence, bio: Faker::Lorem.paragraph)
+end
